fb9a53f945df1c0963f1a6f8a7e0290ab2b7854e
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2019-03-29 09:43:32 -0600

    dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+from subprocess import Popen
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -13,6 +14,8 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/reynoldsnlp/udar',
     packages=setuptools.find_packages(),
+    install_requires=['hfst'],
+    extras_require=['nltk'],
     # dependency_links=['https://github.com/ljos/pyvislcg3'],
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -20,3 +23,26 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
 )
+
+try:
+    Popen(['hfst-info'])
+except FileNotFoundError:
+    print('Command-line hfst not found. In order to use the built-in '
+          'tokenizer, it must be installed, and in your PATH.')
+    print('See http://divvun.no/doc/infra/compiling_HFST3.html\n')
+
+try:
+    import nltk
+    assert nltk.download('punkt')
+except ImportError:
+    print('nltk is not installed, so its tokenizer is not available.')
+    print('Try `python3 -m pip install --user nltk`.\n')
+except AssertionError as e:
+    print('nltk is installed, but tokenizer failed to download.', e)
+    print("Try...\n>>> import nltk\n>>>nltk.download('punkt')\n")
+
+try:
+    Popen(['vislcg3'])
+except FileNotFoundError:
+    print('vislcg3 not found. In order to perform morphosyntactic '
+          'disambiguation, it must be installed, and in your PATH.\n')
