ade398e017f0653f825c9dd4d0a736e8b876ecd9
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2019-07-13 19:29:42 -0600

    add noqa's to __init__

--- udar/__init__.py
+++ udar/__init__.py
@@ -1,10 +1,10 @@
 """Python wrapper of UDAR, a part-of-speech tagger for (accented) Russian"""
 
-from .tag import *
-from .reading import *
-from .tok import *
-from .text import *
-from .fsts import *
+from .tag import *  # noqa: F401, F403
+from .reading import *  # noqa: F401, F403
+from .tok import *  # noqa: F401, F403
+from .text import *  # noqa: F401, F403
+from .fsts import *  # noqa: F401, F403
 
-from .convenience import *
-from .misc import *
+from .convenience import *  # noqa: F401, F403
+from .misc import *  # noqa: F401, F403
