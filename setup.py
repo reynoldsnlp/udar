f14554b43ffad5783bb7806e29971c9501267695
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2020-04-16 14:47:02 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 from setuptools.command.install import install
 from subprocess import Popen
+from sys import stderr
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -10,28 +11,36 @@ class PostInstallCommand(install):
     """Post-installation routine for installation mode."""
     def run(self):
         install.run(self)
+
         try:
             Popen(['hfst-info'])
+            print('HFST appears to be installed correctly.', file=stderr)
         except FileNotFoundError:
             print('Command-line hfst not found. In order to use the built-in '
-                  'tokenizer, it must be installed, and in your PATH.')
-            print('See http://divvun.no/doc/infra/compiling_HFST3.html\n')
+                  'tokenizer, it must be installed, and in your PATH.\n'
+                  'See http://divvun.no/doc/infra/compiling_HFST3.html\n',
+                  file=stderr)
 
         try:
             import nltk
             assert nltk.download('punkt')
+            print('nltk appears to be installed correctly.', file=stderr)
         except ImportError:
-            print('nltk is not installed, so its tokenizer is not available.')
-            print('Try `python3 -m pip install --user nltk`.\n')
+            print('nltk is not installed, so its tokenizer is not available.\n'
+                  'Try `python3 -m pip install --user nltk`.\n', file=stderr)
         except AssertionError as e:
-            print('nltk is installed, but tokenizer failed to download.', e)
-            print("Try...\n>>> import nltk\n>>>nltk.download('punkt')\n")
+            print('nltk is installed, but tokenizer failed to download.', e,
+                  file=stderr)
+            print("Try...\n>>> import nltk\n>>> nltk.download('punkt')\n",
+                  file=stderr)
 
         try:
             Popen(['vislcg3'])
+            print('vislcg3 appears to be installed correctly.', file=stderr)
         except FileNotFoundError:
             print('vislcg3 not found. In order to perform morphosyntactic '
-                  'disambiguation, it must be installed, and in your PATH.\n')
+                  'disambiguation, it must be installed, and in your PATH.\n',
+                  file=stderr)
 
 
 setuptools.setup(
@@ -51,10 +60,29 @@ setuptools.setup(
     install_requires=['hfst', 'nltk'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    # dependency_links=['https://github.com/ljos/pyvislcg3'],
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Framework :: Flake8',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
+        'Natural Language :: Russian',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Education',
+        'Topic :: Education :: Computer Aided Instruction (CAI)',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Utilities',
+        'Typing :: Typed',
     ],
 )
