58ade089160a5464d2456cee0c5bbd9c205f0109
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2020-05-28 14:15:18 -0600

    keep track of dependency versions

--- dev/qa.sh
+++ dev/qa.sh
@@ -1,19 +1,24 @@
+#!/bin/bash
 
-echo -n "Upgrade HFST and vislcg3 (y/n)? "
+rm -f .coverage  # can conflict with tox
+
+echo -n "Upgrade HFST and vislcg3? (y/n) "
 read answer
-
 if [ "$answer" != "${answer#[Yy]}" ] ; then
+    echo "Type password: "
     curl https://apertium.projectjj.com/osx/install-nightly.sh | sudo bash
 else
     echo "Not upgrading hfst and vislcg3."
 fi
 
+echo "Versions with which tests passed for this commit:" \
+    > hfst_vislcg3_versions.txt
 hfst-tokenize --version | grep hfst > hfst_vislcg3_versions.txt
 vislcg3 --version | grep VISL >> hfst_vislcg3_versions.txt
 
 flake8 *.py test/**/*.py udar/**/*.py
 mypy udar
 pytest --cov=udar --cov-append --cov-report term-missing --doctest-modules
-rm .coverage  # can conflict with tox
 
+rm .coverage  # can conflict with tox
 echo "If everything passes, run tox."
