f7c000a72874f5a0dc5e2c2758cbf7b5cd9dd2f8
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-05-25 00:04:33 -0600

    Workaround for pip install -e

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-import setuptools
+import site
+import sys
 
+import setuptools
 import setuptools_scm  # noqa: F401
 import toml  # noqa: F401
+
+site.ENABLE_USER_SITE = '--user' in sys.argv[1:]  # workaround for https://github.com/pypa/pip/issues/7953
+
+
 
 setuptools.setup(package_data={'udar': ['src/udar/resources/*']},
                  setup_requires=['setuptools_scm'],
